6f8991053dc3924ea680f3ec9448758876bffdf1
NormanVetter <vetter.norman@gmail.com>
2014-05-15 18:54:54 +0200

    massive änderungen

---
+++ svManager/server.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+app.set('views',__dirname);
+app.set('view engine', 'jade');
+var http  = require('http').createServer(app)
+, io    = require('socket.io').listen(http) 
+;
+
+http.listen(3000, function(){
+  console.log('Express server listening on port 3000');
+});
+
+app.get('/',function(req,res){res.render('index',{title: 'Choose your view'})});
+
+io.sockets.on('connection', function (socket) {
+    
+    console.log("client was connected");
+
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
